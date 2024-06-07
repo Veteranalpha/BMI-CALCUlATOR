b161536e2bec7f90070843d82dce41b4d890f418
Giwa Mubarak <giwaomogiwa@gmail.com>
2024-06-07 16:28:59 +0100

    included the javascript functionalities

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 'use strict';
-console.log('hello');
-const CalcButton = document.getElementById(calc);
-if (CalcButton == onclick){
-    console.log('ye!')
+const calcButton = document.getElementById('calc');
+
+
+
+calcButton.addEventListener("click",() => {   
+    let heightValue = parseFloat(document.getElementById('height').value);
+let weightValue = parseFloat(document.getElementById('weight').value);
+let Output = document.getElementById('output'); 
+if (heightValue > 0 && weightValue > 0){
+    let bmivalue = weightValue / heightValue ** 2;
+        Output.innerHTML = `The BMI IS ${bmivalue.toFixed(2)}Kg/m^2`
+} else {
+    Output.innerHTML = `Enter a valid Number  and try again!`
 }
+
+});
